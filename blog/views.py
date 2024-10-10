79615cb365198bbc19ab1c4f6340832ea642f410
DESKTOP-52HLHV2\s <katie8454@naver.com>
2024-10-10 17:15:13 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')  # .order_by('-pk') : 최신 글부터 맨위 배치
+
+    return render(
+        request,
+        "blog/index.html",
+        {
+            "posts" : posts,
+        }
+    )
 
 # Create your views here.
+
