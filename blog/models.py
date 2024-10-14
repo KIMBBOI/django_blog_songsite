c5cfa3d344a8cf8dc0fe6993f744c0c3b087b1f6
DESKTOP-52HLHV2\s <katie8454@naver.com>
2024-10-14 11:08:25 +0900

    포스트 상세 페이지에 부트스트랩 적용하기 (코드 수정함)

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30)     #CharField() :  문자열 길이 최대 30으로 제한함.
     content = models.TextField()                #TextField() : 문자열 길이 제한 안둠.
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
     #author: 추후 작성 예정                       #author 필드 : 나중에 모델에서 외래키를 구현할때 다룰것.
