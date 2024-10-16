1d0858352827a0da975bbe2a2344b7e1a9dd78cc
DESKTOP-52HLHV2\s <katie8454@naver.com>
2024-10-16 15:12:39 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -101,83 +101,33 @@ class TestView(TestCase):
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
 
-    # def test_post_list(self):
-    #     # 1. 1 포스트 목록 페이지를 가져온다.
-    #     response = self.client.get('/blog/')
-    #     # 1.2. 정상적으로 페이지가 로드된다
-    #     self.assertEqual(response.status_code, 200)
-    #     # 1.3 페이지 타이틀은 ‘Blog’ 이다.
-    #     soup = BeautifulSoup(response.content, 'html.parser')
-    #     # self.assertEqual(soup.title.text, 'Blog')
-    #
-    #     self.navbar_test(soup)
-    #
-    #     # 2. 1. 포스트(게시물)가 하나도 없다면
-    #     self.assertEqual(Post.objects.count(), 0)
-    #     # 2.2 . main area 에 '아직 게시 물이 없습니다'라는 문구가 나타난다.
-    #     main_area = soup.find('div', id='main-area')
-    #     self.assertIn('아직 게시물이 없습니다', main_area.text)
-    #
-    #     # 3.1 게시물이 2개 있다면
-    #     post_001 = Post.objects.create(
-    #         title='첫번째 포스트입니다.',
-    #         content='Hello World. We are the world.',
-    #         author = self.user_katie
-    #     )
-    #     post_002 = Post.objects.create(
-    #         title='두번째 포스트입니다.',
-    #         content='1등이 전부는 아니잖아요?',
-    #         author = self.user_cold
-    #     )
-    #     self.assertEqual(Post.objects.count(), 2)
-    #
-    #     # 3.2 포스트 목록 페이지를 새로고침 했을 때
-    #     response = self.client.get('/blog/')
-    #     soup = BeautifulSoup(response.content, 'html.parser')
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     # 3.3 '아직 게시물이 없습니다'라는 문구는 더이상 더시상 보이지 않는다.
-    #     main_area = soup.find('div', id = 'main-area')
-    #     self.assertIn(post_001.title, main_area.text)
-    #     self.assertIn(post_002.title, main_area.text)
-    #
-    #     # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 나타나지 않는다.
-    #     self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
-    #
-    #     self.assertIn(self.user_katie.username.upper(), main_area.text)
-    #     self.assertIn(self.user_cold.username.upper(), main_area.text)
-
 
     def test_post_detail(self):
-        # 1.1. 포스트가 하나 있다.
-        post_001 = Post.objects.create(
-            title = '첫번째 포스트입니다.',
-            content = 'Hello World. We are the world.',
-            author = self.user_katie
-        )
 
         # 1. 2. 그 포스트의 url은 '/blog/1/' 이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
         # 2. 첫 번째 포스트의 상세 페이지 테스트
-        # 2. 1. 첫 번째 포스트으I url로 접근하면 정상적으로 작동한다(status code: 200).
-        response = self.client.get(post_001.get_absolute_url())
+        # 2. 1. 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다(status code: 200).
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # 2.2. 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
         # 2.3. 첫 번째 포스트의 제목이 웹 브라우저 탬 타이 에 들어 있다.
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         # 2.4. 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
 
         # 2.5. 첫 번째 포스트의 작성자(author) 가 포스트 영역에 있다(아직 구현할 수 없음)
         self.assertIn(self.user_katie.username.upper(), post_area.text)
 
         # 2.6. 첫 번째 포스트의 (c ontent)OI 포스트 영역에 있다.
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
