5bd9edbb45a714474b610130ca406f5e041bbac5
DESKTOP-52HLHV2\s <katie8454@naver.com>
2024-10-22 17:18:37 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -82,22 +82,9 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # 구글 아바타 설정
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://doitdjango.com/avatar/id/2509/c94d6520730cd566/svg/{self.author.email}'
